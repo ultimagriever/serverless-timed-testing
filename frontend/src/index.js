abdfe96b28252a3ef4e57c4d2fb1845f5755e563
Pamela Argentino <pamela.oliveira@cognizant.com>
2018-09-13 18:09:26 -0300

    Configure Redux and Redux Form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import App from './components/App';
+import reducers from './reducers';
 import './styles/css/index.css';
 
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, document.getElementById('root'));
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>, document.getElementById('root'));
